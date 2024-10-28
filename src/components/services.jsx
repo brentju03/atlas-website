9bfad5c0f6c6cc5d4baa0e43d0ceaf61a794f3b6
brentju03 <brentju@stanford.edu>
2024-10-27 20:12:33 -0700

    fixed navigation bug where video link was not working

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 export const Services = (props) => {
   return (
-    <div id="services" className="text-center">
+    <div id="video" className="text-center">
       <div className="container">
         <div className="section-title">
           <h2>Concept Video</h2>
-          <iframe width="750" height="400" 
-            src="https://www.youtube.com/embed/PL-9t2C86RQ" 
-            title="Video title" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen>
-          </iframe>
+          <iframe
+            width="750"
+            height="400"
+            src="https://www.youtube.com/embed/PL-9t2C86RQ"
+            title="Video title"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
